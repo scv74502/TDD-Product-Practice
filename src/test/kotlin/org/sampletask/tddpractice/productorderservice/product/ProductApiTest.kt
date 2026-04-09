4a49657700fe5855de119d396da2d86e4801fc81
kunwoo park <scv74502@gmail.com>
2026-04-09 23:27:18 +0900

    [refactor] 디렉토리 이동

--- src/test/kotlin/org/sampletask/tddpractice/productorderservice/product/ProductApiTest.kt
+++ src/test/kotlin/org/sampletask/tddpractice/productorderservice/product/ProductApiTest.kt
@@ -1,17 +1,12 @@
-package org.sampletask.tddpractice.productorderservice.service
+package org.sampletask.tddpractice.productorderservice.product
 
 import io.restassured.RestAssured
 import io.restassured.response.ExtractableResponse
 import io.restassured.response.Response
-import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
-import org.sampletask.tddpractice.productorderservice.product.AddProductRequest
-import org.sampletask.tddpractice.productorderservice.product.DiscountPolicy
-import org.sampletask.tddpractice.productorderservice.product.ProductService
-import org.sampletask.tddpractice.productorderservice.product.ProductSteps
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 
 class ProductApiTest : ProductSteps() {
     @Autowired
@@ -25,12 +20,12 @@ class ProductApiTest : ProductSteps() {
         val response =
             `상품등록요청`(request)
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
     }
 
     @Test
     fun `상품조회`() {
-        ProductSteps.상품등록요청(ProductSteps.상품등록요청_생성())
+        상품등록요청(상품등록요청_생성())
         val productId: Long = 1L
 
         val response: ExtractableResponse<Response> =
@@ -45,7 +40,7 @@ class ProductApiTest : ProductSteps() {
                 .all()
                 .extract()
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
-        assertThat(response.jsonPath().getString("name")).isEqualTo("상품명")
+        Assertions.assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+        Assertions.assertThat(response.jsonPath().getString("name")).isEqualTo("상품명")
     }
 }
