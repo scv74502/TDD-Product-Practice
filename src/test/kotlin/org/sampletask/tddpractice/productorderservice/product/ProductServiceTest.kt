2bc6f1092e427097ef95e402f4f95bcd8ca88aa8
kunwoo park <scv74502@gmail.com>
2026-04-13 16:00:15 +0900

    [fix] ProductServiceTest 에러 수정

--- src/test/kotlin/org/sampletask/tddpractice/productorderservice/product/ProductServiceTest.kt
+++ src/test/kotlin/org/sampletask/tddpractice/productorderservice/product/ProductServiceTest.kt
@@ -10,22 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ProductServiceTest {
     @Autowired
-    private lateinit var productPort: ProductPort
-    @Autowired
     private lateinit var productService: ProductService
 
-    @BeforeEach
-    fun setUp() {
-        val productPort = Mockito.mock(ProductPort::class.java)
-        productService = ProductService(productPort)
-    }
-
-
     @Test
-    fun `상품수정`(){
-        productService.addProduct(ProductSteps.상품등록요청_생성())
+    fun `상품수정`() {
+        val product = productService.addProduct(ProductSteps.상품등록요청_생성())
         val productId = 1L
-        val request = ProductSteps. `상품수정요청_생성`()
+        val request = ProductSteps.`상품수정요청_생성`()
 
         productService.updateProduct(productId, request)
 
@@ -34,15 +25,4 @@ class ProductServiceTest {
         assertThat(productResponse.name).isEqualTo("상품 수정")
         assertThat(productResponse.price).isEqualTo(2000)
     }
-
-    //    private class StubProductPort : ProductPort {
-//        lateinit var getProduct_will_return: Product
-//        override fun save(product: Product) {
-//
-//        }
-//
-//        override fun getProduct(productId: Long): Product {
-//            return getProduct_will_return
-//        }
-//    }
 }
